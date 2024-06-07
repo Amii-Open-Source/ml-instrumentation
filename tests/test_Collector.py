185ee1418da71a71c2033a2c6d44b1ce129367d9
Andy Patterson <andnpatterson@gmail.com>
2024-06-07 13:09:57 -0600

    refactor: make sure collector is cleaned after test

--- tests/test_Collector.py
+++ tests/test_Collector.py
@@ -1,14 +1,20 @@
+import pytest
 from ml_instrumentation.Collector import Collector
 from ml_instrumentation.Sampler import Identity, Ignore
 
-def test_collector1():
-    collector = Collector(
+@pytest.fixture
+def collector():
+    c = Collector(
+        tmp_file=':memory:',
         config = {
             'm1': Identity()
         },
         default=Ignore()
     )
+    yield c
+    c.close()
 
+def test_collector1(collector):
     assert collector.keys() == set()
     assert collector.experiment_ids() == set()
 
