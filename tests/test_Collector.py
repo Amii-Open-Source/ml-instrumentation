3ee6ec3d24d2a85fc0d2e33520e69ec2f80b9439
Andy Patterson <andnpatterson@gmail.com>
2024-06-08 16:40:20 -0600

    test: add basic read/write test

--- tests/test_Collector.py
+++ tests/test_Collector.py
@@ -1,22 +1,46 @@
 import pytest
 from ml_instrumentation.Collector import Collector
 from ml_instrumentation.Sampler import Identity, Ignore
+from ml_instrumentation.Writer import SqlPoint
 
 @pytest.fixture
 def collector():
     c = Collector(
         tmp_file=':memory:',
         config = {
-            'm1': Identity()
+            'm1': Identity(),
+            'm2': Identity(),
         },
         default=Ignore()
     )
     yield c
     c.close()
 
-def test_collector1(collector):
+def test_collector_setup1(collector):
     assert collector.keys() == set()
     assert collector.experiment_ids() == set()
 
     # non-existent metrics and/or experiment_ids should give empty lists
     assert collector.get('m1', 0) == []
+
+def test_collector_rw1(collector):
+    collector.set_experiment_id(0)
+    collector.next_frame()
+
+    collector.collect('m1', 0)
+    assert collector.get('m1', 0) == [
+        SqlPoint(frame=0, id=0, measurement=0)
+    ]
+
+    collector.next_frame()
+    collector.collect('m1', 2)
+    collector.collect('m2', 1)
+    assert collector.get('m1', 0) == [
+        SqlPoint(frame=0, id=0, measurement=0),
+        SqlPoint(frame=1, id=0, measurement=2),
+    ]
+
+    assert collector.get('m2', 0) == [
+        SqlPoint(frame=1, id=0, measurement=1),
+    ]
+
