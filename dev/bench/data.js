37a74096ca01d3eaca39a49f1334641d96cc04a8
github-action-benchmark <github@users.noreply.github.com>
2024-06-17 18:44:39 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 8439febe6f545365174201861dc2084d6ec50b15

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718298161664,
+  "lastUpdate": 1718649879413,
   "repoUrl": "https://github.com/Amii-Open-Source/ml-instrumentation",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -52,6 +52,58 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0007938075758886335",
             "extra": "mean: 2.755096790760957 msec\nrounds: 368"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "andnpatterson@gmail.com",
+            "name": "Andy Patterson",
+            "username": "andnp"
+          },
+          "committer": {
+            "email": "160795226+andy-amii@users.noreply.github.com",
+            "name": "andy-amii",
+            "username": "andy-amii"
+          },
+          "distinct": true,
+          "id": "8439febe6f545365174201861dc2084d6ec50b15",
+          "message": "chore: use numpy 2.0 migration ruff rule",
+          "timestamp": "2024-06-17T12:44:14-06:00",
+          "tree_id": "033002aba43a7fe9739deef8d97dfd0cf54ed885",
+          "url": "https://github.com/Amii-Open-Source/ml-instrumentation/commit/8439febe6f545365174201861dc2084d6ec50b15"
+        },
+        "date": 1718649879011,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/performance/test_Collector.py::test_benchmark_write_path1[basic_collector]",
+            "value": 143.16270968817219,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0017551819055799803",
+            "extra": "mean: 6.985059183205848 msec\nrounds: 131"
+          },
+          {
+            "name": "tests/performance/test_Collector.py::test_benchmark_write_path1[disk_collector]",
+            "value": 96.77237952375211,
+            "unit": "iter/sec",
+            "range": "stddev: 0.002402241308949498",
+            "extra": "mean: 10.333527034483605 msec\nrounds: 58"
+          },
+          {
+            "name": "tests/performance/test_Collector.py::test_benchmark_read1[basic_collector]",
+            "value": 364.10920336819726,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0009785960796770926",
+            "extra": "mean: 2.746428793201287 msec\nrounds: 353"
+          },
+          {
+            "name": "tests/performance/test_Collector.py::test_benchmark_read1[disk_collector]",
+            "value": 354.8489703477283,
+            "unit": "iter/sec",
+            "range": "stddev: 0.001054841824145278",
+            "extra": "mean: 2.818100328768227 msec\nrounds: 365"
           }
         ]
       }
