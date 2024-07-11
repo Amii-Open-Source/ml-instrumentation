e2a1dd42995b0aab1ee65fb941fd2599235f23d0
andnp <andnpatterson@gmail.com>
2024-07-11 12:32:19 -0600

    feat: add ability to merge collectors across processes

--- tests/integration/test_Writer.py
+++ tests/integration/test_Writer.py
@@ -1,4 +1,7 @@
-from ml_instrumentation.Writer import Point, SqlPoint
+from functools import partial
+import sqlite3
+from ml_instrumentation.Writer import Point, SqlPoint, Writer
+from multiprocessing.pool import Pool
 
 def test_write1(writer):
     d = Point(
@@ -51,3 +54,73 @@ def test_write2(writer):
 
     points = writer.read_metric('measurement-2')
     assert len(points) == 334
+
+
+def test_merge1(tmp_path):
+    w1 = Writer(
+        db_path=str(tmp_path / 'w1.db'),
+    )
+
+    w2 = Writer(
+        db_path=':memory:',
+    )
+
+    for i in range(10):
+        w1.write(Point(exp_id=0, metric='a', frame=i, data=i))
+        w2.write(Point(exp_id=1, metric='a', frame=i, data=100+i))
+
+    w1.merge(str(tmp_path / 'total.db'))
+    w2.merge(str(tmp_path / 'total.db'))
+
+    con = sqlite3.connect(tmp_path / 'total.db')
+    cur = con.cursor()
+
+    cur.execute('SELECT * FROM a WHERE id=0 ORDER BY frame')
+    res = cur.fetchall()
+    assert len(res) == 10
+    assert [r[2] for r in res] == [i for i in range(10)]
+
+    cur.execute('SELECT * FROM a WHERE id=1 ORDER BY frame')
+    res = cur.fetchall()
+    assert len(res) == 10
+    assert [r[2] for r in res] == [100+i for i in range(10)]
+
+    cur.execute('SELECT * FROM a')
+    res = cur.fetchall()
+    assert len(res) == 20
+
+    w1.close()
+    w2.close()
+
+
+def test_merge_parallel1(tmp_path):
+    pool = Pool(10)
+    pool.map(partial(_test_merge_parallel1, tmp_path=tmp_path), range(20))
+
+    con = sqlite3.connect(tmp_path / 'total.db')
+    cur = con.cursor()
+
+    for i in range(20):
+        cur.execute(f'SELECT * FROM a WHERE id={i} ORDER BY frame')
+        res = cur.fetchall()
+        assert len(res) == 100
+        assert [r[2] for r in res] == [2*j + i for j in range(100)]
+
+        cur.execute(f'SELECT * FROM b where id={i} ORDER BY frame')
+        res = cur.fetchall()
+        assert len(res) == 100
+        assert [r[2] for r in res] == [f'{j} - {i}' for j in range(100)]
+
+    cur.execute('SELECT * FROM a')
+    res = cur.fetchall()
+    assert len(res) == 20 * 100
+
+
+def _test_merge_parallel1(i: int, tmp_path):
+    writer = Writer(db_path=str(tmp_path / f'w{i}.db'))
+
+    for j in range(100):
+        writer.write(Point(exp_id=i, metric='a', frame=j, data=2*j + i))
+        writer.write(Point(exp_id=i, metric='b', frame=j, data=f'{j} - {i}'))
+
+    writer.merge(str(tmp_path / 'total.db'))
